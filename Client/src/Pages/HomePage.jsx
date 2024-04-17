ea0385a486ede69a901a204828938f81ee65e871
Govind ghosh <govindghosh0@gmail.com>
2024-04-17 19:27:19 +0530

    [FIX] ui HomePage

--- Client/src/Pages/HomePage.jsx
+++ Client/src/Pages/HomePage.jsx
@@ -2,69 +2,51 @@ import {
   Card,
   CardBody,
   CardHeader,
+  Flex,
   Grid,
   Heading,
   SimpleGrid,
   Text,
+  VStack,
+  Box,
+  Center, // Import Center component
 } from "@chakra-ui/react";
 import useGetUserTask from "../Hooks/useGetUserTask";
 
 function HomePage() {
   const { isLoading, todos } = useGetUserTask();
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  console.log(todos);
+
   return (
-    <>
-      {/* <div>
-        {todos.map((todo) => (
-          <div key={todo._id}>
-            <Text as="b">{todo?.title}</Text>
-            <br />
-          </div>
-        ))}
-      </div> */}
-      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-        {todos.map((todo) => (
-          <SimpleGrid
-            key={todo._id}
-            spacing={4}
-            templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-            p={"2px"}
-            maxW={"full"}
-          >
-            <Card>
-              <CardHeader>
-                <Heading size="md"> {todo?.title}</Heading>
-              </CardHeader>
-              <CardBody>
-                {todo.completed ? (
-                  <Text as="s">{todo?.description}</Text>
-                ) : (
-                  <Text>{todo?.description}</Text>
-                )}
-              </CardBody>
-            </Card>
-          </SimpleGrid>
-        ))}
-      </Grid>
-      <Text as="b">Bold</Text>
-      <br />
-      <Text as="u">Underline</Text>
-      <br />
-      <Text as="del">Deleted</Text>
-      <br />
-      <Text as="em">Emphasis</Text>
-      <br />
-      <Text as="ins">Inserted</Text>
-      <br />
-      <Text as="kbd">Ctrl + C</Text>
-      <br />
-      <Text as="mark">Highlighted</Text>
-      <br />
-      <Text as="s">Strikethrough</Text>
-    </>
+    <Flex direction="column" h="100vh">
+      <Center color="white" h="100px">
+        This is the Center
+      </Center>
+
+      <Flex flex="1">
+        <VStack w="100%" p={4} spacing={6}>
+          <Grid templateColumns={{ base: "1fr", md: "repeat(4, 1fr)" }} gap={6}>
+            {todos.map((todo) => (
+              <SimpleGrid key={todo._id} spacing={4} p={"2px"} maxW={"full"}>
+                <Card>
+                  <CardHeader>
+                    <Heading size="md">{todo?.title}</Heading>
+                  </CardHeader>
+                  <CardBody>
+                    <Text as={todo.completed ? "s" : "span"}>
+                      {todo?.description}
+                    </Text>
+                  </CardBody>
+                </Card>
+              </SimpleGrid>
+            ))}
+          </Grid>
+        </VStack>
+      </Flex>
+    </Flex>
   );
 }
 
